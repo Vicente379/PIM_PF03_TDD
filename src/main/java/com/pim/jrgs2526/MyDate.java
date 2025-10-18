298559c7b1eadf9dcbc4054e84586e4fba1dd106
Vicente379 <vicentegarciafernandez43@gmail.com>
2025-10-18 21:46:48 +0200

    Test github workflow

--- src/main/java/com/pim/jrgs2526/MyDate.java
+++ src/main/java/com/pim/jrgs2526/MyDate.java
@@ -1,11 +1,68 @@
 package com.pim.jrgs2526;
 
+import java.time.LocalDate;
+import java.time.YearMonth;
+
 public class MyDate {
 
     public static final String ERR_INVALID_YEAR = "Year value not valid";
     public static final String ERR_INVALID_MONTH = "Month value not valid";
     public static final String ERR_INVALID_DAY = "Day value not valid";
     public static final String ERR_INVALID_DATE = "Invalid date";
+
+    private int day;
+    private Months month;
+    private int year;
+
+    public MyDate(int day, Months month, int year) {
+        if (year < 0) throw new IllegalArgumentException(ERR_INVALID_DATE);
+
+        int maxDaysInMonth = YearMonth.of(year, month.monthNumber).lengthOfMonth();
+        if (day > maxDaysInMonth) throw new IllegalArgumentException(ERR_INVALID_DATE);
+
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public MyDate() {
+
+    }
+
+    public void setMonth(Months month) {
+        if (this.year < 1 || this.day < 1) throw new IllegalArgumentException(ERR_INVALID_MONTH);
+
+        if (this.day > getLastDayOfTheMonth(month) ) throw new IllegalArgumentException(ERR_INVALID_MONTH);
+
+        this.month = month;
+    }
+
+    private int getLastDayOfTheMonth(Months month) {
+        if (this.year != 0) return 0;
+
+        return YearMonth.of(year, month.monthNumber).lengthOfMonth();
+    }
+
+    public void setYear(int year) {
+
+        if (year <= 0 || year > LocalDate.now().getYear() || !isLeapYear(year)) {
+            throw new IllegalArgumentException(ERR_INVALID_YEAR);
+        }
+        this.year = year;
+    }
+
+    private boolean isLeapYear(int year) {
+        return (year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0));
+    }
+
+    public void setDay(int day) {
+
+        if (day <= 0 || day > LocalDate.now().getDayOfMonth()) {
+            throw new IllegalArgumentException(ERR_INVALID_DAY);
+        }
+        this.day = day;
+    }
+
 
     public enum Months {
         JANUARY(1),
