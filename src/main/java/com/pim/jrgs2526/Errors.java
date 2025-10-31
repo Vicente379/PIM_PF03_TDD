c6cace458672d0c58e196af64869726d627abacf
Vicente379 <vicentegarciafernandez43@gmail.com>
2025-11-01 00:46:11 +0100

    Add Errors Class

--- src/main/java/com/pim/jrgs2526/Errors.java
+++ src/main/java/com/pim/jrgs2526/Errors.java
@@ -5,5 +5,5 @@ public class Errors {
     public static final String ERR_INVALID_MONTH = "Month value not valid";
     public static final String ERR_INVALID_DAY = "Day value not valid";
     public static final String ERR_INVALID_DATE = "Invalid date";
-    
+
 }
